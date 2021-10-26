9d4b48371594c61a87d22787fbc1fd6d8fdd872f
maxb0507 <maxb0507@student.ubc.ca>
2021-10-25 20:02:31 -0700

    Added more to my function

--- notebooks/project_functions1.py
+++ notebooks/project_functions1.py
@@ -10,15 +10,23 @@ def unprocessed(csv_file):
 
 
 def load_and_process(csv_file):
-    dfx = pd.read_csv(csv_file, sep=";")
+    df = pd.read_csv(csv_file, sep=";")
     df1=(
-        dfx.drop(["Date_of_birth", "TEAM", "Place_of_birth", "Saves", "Save_Percentage", "Shutouts", "Goals_Against", "Goals_Against_Average", "Shots_Against",
+        df.drop(["Date_of_birth", "TEAM", "Place_of_birth", "Saves", "Save_Percentage", "Shutouts", "Goals_Against", "Goals_Against_Average", "Shots_Against",
                     "Time_on_Ice_per_Game", 'Games_Started', 'Number', "Overtime_Losses", "Ties", "PowerPlay_Goals", "PowerPlay_Assists", "Short_Goals",
                     "Short_Assists", "Game_Tying_Goals", "SEASON", "Game_Winning_Goals", "Production", "Wins", "Losses", "Experience", "PlusMinus_Ratings", "Penalty_Minutes",
                     "Position"], axis=1))
-    df2=(
-        df1.drop(df1[df1["SEASON_year"] < 2000].index))
-    return df2
+    df2 = (
+        df1.drop(df[df["SEASON_year"] < 2000].index)
+           .reset_index(drop=True))
+    
+    df3 = (
+        df2.dropna()
+           .reset_index(drop=True))
+    df4 = (
+        df3.groupby(["Name","Height","Weight","Body_mass_index"],as_index=False)[["Games_Played","Goals", "Assists", "Shots_on_Goal", "Points"]].sum())
+   
+    return df4
 
     # etc...
 
