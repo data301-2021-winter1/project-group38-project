ce83ba104a1805081f7032ba2ad0456a1233ea77
maxb0507 <maxb0507@student.ubc.ca>
2021-10-25 20:17:19 -0700

    Created another function for topScorers

--- notebooks/project_functions1.py
+++ notebooks/project_functions1.py
@@ -8,6 +8,7 @@ def unprocessed(csv_file):
     df = pd.read_csv(csv_file, sep=";")
     return df
 
+# Method Chain 2 (Create new columns, drop others, and do processing)
 
 def load_and_process(csv_file):
     df = pd.read_csv(csv_file, sep=";")
@@ -28,8 +29,33 @@ def load_and_process(csv_file):
    
     return df4
 
-    # etc...
+    # Creates Dataframe with top Scorers
 
-    # Method Chain 2 (Create new columns, drop others, and do processing)
+def topScorers(csv_file):
+    df = pd.read_csv(csv_file, sep=";")
+    df1=(
+        df.drop(["Date_of_birth", "TEAM", "Place_of_birth", "Saves", "Save_Percentage", "Shutouts", "Goals_Against", "Goals_Against_Average", "Shots_Against",
+                    "Time_on_Ice_per_Game", 'Games_Started', 'Number', "Overtime_Losses", "Ties", "PowerPlay_Goals", "PowerPlay_Assists", "Short_Goals",
+                    "Short_Assists", "Game_Tying_Goals", "SEASON", "Game_Winning_Goals", "Production", "Wins", "Losses", "Experience", "PlusMinus_Ratings", "Penalty_Minutes",
+                    "Position"], axis=1))
+    df2 = (
+        df1.drop(df[df["SEASON_year"] < 2000].index)
+           .reset_index(drop=True))
+    
+    df3 = (
+        df2.dropna()
+           .reset_index(drop=True))
+    df4 = (
+        df3.groupby(["Name","Height","Weight","Body_mass_index"],as_index=False)[["Games_Played","Goals", "Assists", "Shots_on_Goal", "Points"]].sum())
+   
+    df4["Succesful Players"]=df4["Goals"]>300
+   
+    df5 = (
+        df4.drop(df4[df4["Succesful Players"] == False].index)
+           .reset_index(drop=True))
+   
+    return df5
+
+    
 
     # Make sure to return the latest dataframe
