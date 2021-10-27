d99e0423e6759cb6912a41bfde9414b3b6643db8
MattHansen1 <matt.hansen18@yahoo.com>
2021-10-26 22:04:17 -0700

    Still need to finish the functions

--- notebooks/project_functions2.py
+++ notebooks/project_functions2.py
@@ -28,13 +28,14 @@ def mvp_seasons(csv_file):
     'SEASON_year': [2018, 2017, 2016, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2004, 2003, 2001],
     'MVP': ['Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes']
 }
+    mvp_df = pd.DataFrame(mvp_dict)
     df1 = (
         df.drop(df.columns[[1,3,4,10,11,12,13,14,15,16,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,36,37]], axis=1))
     df2 = (
         df1.drop(df[df["SEASON_year"] < 1988].index)
            .reset_index(drop=True))
     df3 = (
-        pd.merge(df2, mvp_dict)
+        pd.merge(df2, mvp_df))
     df4 = (
         df3.mergeby(["Name", "SEASON_year", "Position", "Height", "Weight", "Age", "Experience", "MVP"],as_index=False)[["Games_Played","Goals", "Assists","Points", "PlusMinus_Ratings", "Game_Winning_Goals"]].sum())
         
